9fa128774dd4cac6a4fa751d934c35ea85afa121
10mmason <10mmason@ashmanorschool.com>
2014-03-04 14:30:13 +0000

    Create ASCII.py

---
+++ ASCII.py
@@ -0,0 +1,19 @@
+print ('a' in 'boat')
+#Expected outcome is
+print ('c in 'boat')
+#Expected outcome is
+print ('2' + '2')
+#Expected outcome is
+print (4*'4')
+#Expected outcome is
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(len(alphabet))
+#Expected outcome is
+print(alphabet[1])
+#Expected outcome is
+print(alphabet[4:7]
+#Expected outcome is
+print(alphabet[0:6:2]
+#Expected outcome is
+print(alphabet[::-1])
+#Expected outcome is
