6b1610d7deadc25997516771c12597c401f102a1
10mmason <10mmason@ashmanorschool.com>
2014-03-05 10:22:13 +0000

    Update Looping.py

--- Looping.py
+++ Looping.py
@@ -1,1 +1,10 @@
 def wordforloop(word)
+    for letter in word:
+        print (letter)
+        print (" ")
+
+words = ['hello','year','ten']
+
+for word in words:
+    wordforloop(word)
+    print('-----')
