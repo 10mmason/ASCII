688bcc814eed2b8c0b7ae4f6450158ca0d968718
OpenShift guest <10mmason@ashmanorschool.com>
2014-03-05 05:13:27 -0500

    hello

--- ASCII.py
+++ ASCII.py
@@ -1,19 +1,19 @@
 print ('a' in 'boat')
-#Expected outcome is
+#Expected outcome is true
 print ('c' in 'boat')
-#Expected outcome is
+#Expected outcome is false
 print ('2' + '2')
-#Expected outcome is
+#Expected outcome is 22
 print (4*'4')
-#Expected outcome is
+#Expected outcome is 4444
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 print(len(alphabet))
-#Expected outcome is
+#Expected outcome is 26
 print(alphabet[1])
-#Expected outcome is
-print(alphabet[4:7]
-#Expected outcome is
-print(alphabet[0:6:2]
-#Expected outcome is
-print(alphabet[::-1])
-#Expected outcome is
+#Expected outcome is b
+print(alphabet[4:7])
+#Expected outcome is efg
+print(alphabet[0:6:2]) 
+#Expected outcome is ace
+print(alphabet[::-1]) 
+#Expected outcome is zyxwvutsrpqonmlkjihgfedcba
